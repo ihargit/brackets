ac53a3df3d0587643f5ce721f3914d5d6658eac8
ihargit <ihar.kharashun@gmail.com>
2019-03-14 14:34:16 +0300

    feat: add code to index.js

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Brackets
+ # Brackets
 
 ## Task
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+  let brackets = [];
+  for (let bracket in str) {
+    for (let bracketPair in bracketsConfig) {
+      if (brackets[brackets.length - 1] === bracketPair[0] && bracket === bracketPair[1]) {
+        brackets.pop();
+        break;
+      } else {
+        brackets.push(bracket);
+      }
+    }
+  }
+  console.log(brackets);
+  if (brackets.length > 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
