8e3de44d52f3403cec86e8518be1c2a4c55ea286
ihargit <ihar.kharashun@gmail.com>
2019-03-14 16:03:04 +0300

    fix: fix bugs, so it pass all the tests

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let brackets = []; // array (stack) to store characters
+  let popped; // shows if character was added to the stack array
 
-  let brackets = [];
+  // iterate through string
   for (let i = 0; i < str.length; i++) {
+    popped = false;
+
+    // iterate through bracketsConfig array
     for (let b = 0; b < bracketsConfig.length; b++) {
+      //check if there is coincidence between the last stack character, current string character and the subconfig
       if (brackets[brackets.length - 1] === bracketsConfig[b][0] && str[i] === bracketsConfig[b][1]) {
+        //delete character from the stack
         brackets.pop();
+        popped = true;
         break;
-      } else {
-        brackets.push(str[i]);
       }
     }
+
+    if (popped === false) {
+      //add character to the stack
+      brackets.push(str[i]);
+    }
   }
-  console.log(brackets);
+
   if (brackets.length > 0) {
     return false;
   } else {
